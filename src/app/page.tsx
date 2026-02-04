53d9b03c9f290a3c24e3e201bcbae6fdbd732d3d
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2026-02-03 23:42:51 -0500

    feat(app): hello world next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     hello world
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">
+        Hola, Next.js 🚀
+      </h1>
+      <p className="text-lg mt-4 text-gray-600">
+        Bienvenido al repo learn-next
+      </p>
+    </main>
   );
 }
